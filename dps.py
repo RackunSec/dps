3dcbe0f1ff436cea995f4cf89656473c4acb1d06
Douglas Berdeaux <weaknetlabs@gmail.com>
2020-04-13 12:49:45 +0000

    Ditched cmd2
    cmd2 does not fit this project any longer. This is now done from scratch.

--- dps.py
+++ dps.py
@@ -5,111 +5,72 @@
 #
 # 2020 - Douglas Berdeaux
 #
+import readline
 import os # for the commands, of course. These will be passed ot the shell.
 import subprocess # for piping commands
 import sys # for exit
 import re # regexps
-from cmd2 import Cmd # The actual command-line lib
-from cmd2 import style, fg, bg
 import ifaddr # NIC info
 import socket # for HOSTNAME
 import getpass # for logging the username
 import datetime # for logging the datetime
 
-# constants:
 ADAPTERS = ifaddr.get_adapters() # get network device info
 NET_DEV = "" # store the network device
 LOG_FILENAME = os.path.expanduser("~")+"/.log_dps_history.csv"
 HOSTNAME = socket.gethostname() # hostname for logging
 UID = getpass.getuser()
+REDIRECTION_PIPE = '_'
 
-# Check if log file exists
-if not os.path.exists(LOG_FILENAME):
-    with open(LOG_FILENAME, 'w+') as new_log_file:
-        new_log_file.write("Date,Hostname,Network,Who,Where,Command\n")
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-for adapter in ADAPTERS: # loop over the adapters object
-    if re.match("^e..[0-9]",adapter.nice_name): # looking for ethernet device here
-        NET_DEV = adapter.nice_name+":"+str(adapter.ips[0].ip)
-        break # we got what we need.
+readline.parse_and_bind('tab: complete')
+readline.parse_and_bind('set editing-mode vi')
 
 def log_cmd(cmd): # logging a command:
-    #os.system("echo $(date),$(hostname),"+str(NET_DEV)+",$(whoami),"+cmd+" >> "+LOG_FILENAME)
     with open(LOG_FILENAME,'a') as log_file:
         log_file.write(str(datetime.datetime.now())+","+HOSTNAME+","+str(NET_DEV)+","+UID+","+os.getcwd()+","+cmd+"\n")
     return 0
 
 def run_cmd(cmd):
-    os.system(cmd)
+    if re.match("^cd\s",cmd):
+        dir = re.sub('^cd\s+','',cmd)
+        os.chdir(dir)
+    else:
+        os.system(cmd)
     return 0
 
-class REPL(Cmd): # Read Eval Print Loop
+def list_folder(path):
+    """
+    Lists folder contents
+    """
+    if path.startswith(os.path.sep):
+        # absolute path
+        basedir = os.path.dirname(path)
+        contents = os.listdir(basedir)
+        # add back the parent
+        contents = [os.path.join(basedir, d) for d in contents]
+    else:
+        # relative path
+        contents = os.listdir(os.curdir)
+    return contents
 
-    prompt = UID+"@"+HOSTNAME+" ["+os.getcwd()+"] >> "
 
-    def __init__(self):
-        Cmd.__init__(self)
-        super().__init__(use_ipython=True)
-        # Intro Banner:
-        self.intro = style('\n *** Welcome to the Demon Pentest Shell\n *** hit CTRL+D to exit to standard shell.\n'+
-            ' *** type cmd <command> to run basic shell commands.\n', bold=True)
-        self.continuation_prompt="'-> "
-        # Auto complete things, that don't do shit:
-        self.allow_closing_quote = True
-        self.allow_appended_space = True
-        self.matches_delimited = False
-    ###
-    ### This section is for adding tab autocomplete to basic commands:
-    ###
-    def do_cat(self,line):
-        """[?] Display file contents into the terminal."""
-        cmd = "cat "+line
-        log_cmd(cmd)
-        run_cmd(cmd)
-    complete_cat=Cmd.path_complete
+def completer(text, state):
+    """
+    Our custom completer function
+    """
+    options = [x for x in list_folder(text) if x.startswith(text)]
+    return options[state]
 
-    def do_ls(self,line):
-        """[?] Display directory contents into the terminal."""
-        cmd = "ls "+line
-        log_cmd(cmd)
-        run_cmd(cmd)
-    complete_ls=Cmd.path_complete
-
-    def do_pip3(self,line):
-        """[?] Python 3 PIP module installation."""
-        cmd = "pip3 "+line
-        log_cmd(cmd)
-        run_cmd(cmd)
-    complete_pip3=Cmd.path_complete
-
-    def do_cd(self,line):
-        """[?] Change Directory. If space is in name, end with a double quote, \"."""
-        cmd = "cd "+line
-        log_cmd(cmd)
-        nwd = re.sub(r'["]','',line)
-        if (nwd == ""):
-            nwd = os.path.expanduser('~')
-        os.chdir(nwd)
-        Cmd.async_update_prompt(self,UID+"@"+HOSTNAME+" ["+os.getcwd()+"] >> ")
-        print("\n")
-    complete_cd=Cmd.path_complete
-
-    def do_x(self,line):
-        """[?] Execute shell command. Adds TAB-autocompletion of file names."""
-        # in this method, we don't log "x"
-        log_cmd(line)
-        run_cmd(line)
-    complete_x = Cmd.path_complete
-
-    def default(self,line):
-        log_cmd(line.raw) # log it first:
-        run_cmd(line.raw) # run it.
-    # complete_default = Cmd.path_complete # doesn't work, unfortunately.
-
-    def do_exit(self,line):
-        """[?] Exit the Demon Pentest Shell."""
-        sys.exit()
-
-if __name__ == '__main__':
-    app = REPL()
-    app.cmdloop()
+readline.set_completer(completer)
+readline.parse_and_bind('tab: complete')
+readline.set_completer_delims(' \t\n`~!@#$%^&*()-=+[{]}\\|;:\'",<>?')
