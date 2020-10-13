6646413aaa76f120fe5a0ab42280c47cf2ef0165
weaknetlabs <weaknetlabs@gmail.com>
2020-10-13 12:31:09 -0400

    DPS.INI file, styles, and dps_config command built-in

--- dps.py
+++ dps.py
@@ -5,7 +5,7 @@
 #
 # 2020 - Douglas Berdeaux
 
-import readline
+import configparser # dps.conf from ~/.dps/
 import os # for the commands, of course. These will be passed ot the shell.
 import subprocess # for piping commands
 import sys # for exit
@@ -16,31 +16,25 @@ import getpass # for logging the username
 import datetime # for logging the datetime
 from prompt_toolkit import prompt, ANSI # for input
 from prompt_toolkit.completion import WordCompleter # completer function (feed a list)
+from prompt_toolkit import PromptSession
 
+###===========================================
+## GLOBAL VALUES:
+###===========================================
 ADAPTERS = ifaddr.get_adapters() # get network device info
 NET_DEV = "" # store the network device
 HOSTNAME = socket.gethostname() # hostname for logging
 UID = getpass.getuser() # Get the username
-REDIRECTION_PIPE = '_'
+REDIRECTION_PIPE = '_' # TODO not needed?
 VERSION = "v0.10.7-2" # update this each time we push to the repo
 LOG_DAY = datetime.datetime.today().strftime('%Y-%m-%d') # get he date for logging purposes
 LOG_FILENAME = os.path.expanduser("~")+"/.dps/"+LOG_DAY+"_dps_log.csv" # the log file is based on the date
+CONFIG_FILENAME = os.path.expanduser("~")+"/.dps/dps.ini" # config (init) file name
+CONFIG = configparser.ConfigParser() # config object
 OWD=os.getcwd() # historical purposes
 # Add all built-in commands here so they populate in the tab-autocompler:
-BUILTINS=['dps_stats','dps_uid_gen','dps_wifi_mon']
-PRMPT_CLR=0 # Prompt Color setting
-
-# Set up the log file directory:
-if not os.path.exists(os.path.join(os.path.expanduser("~"),".dps")): # create the directory if it does not exist
-    os.mkdir(os.path.join(os.path.expanduser("~"),".dps")) # mkdir
-# Set up the log file itself:
-if not os.path.exists(LOG_FILENAME):
-    with open(LOG_FILENAME,'a') as log_file:
-        log_file.write("When,Host,Network,Who,Where,What\n")
-# Get the adapter and IP address:
-for adapter in ADAPTERS: # loop through adapters
-    if re.match("^e..[0-9]+",adapter.nice_name):
-        NET_DEV = adapter.nice_name+":"+adapter.ips[0].ip
+BUILTINS=['dps_stats','dps_uid_gen','dps_wifi_mon','dps_config']
+PRMPT_STYL=0 # Prompt style setting
 # colored output (does not work with the prompt - causes issues with line wrapping)
 class bcolors:
     HEADER = '\033[95m'
@@ -57,21 +51,39 @@ class bcolors:
     LGHTGRY='\033[37m'
     ORNG = '\e033[220m'
 
-    #readline.parse_and_bind('set editing-mode vi')
-    #readline.parse_and_bind('set horizontal-scroll-mode On') # will scroll horizontally, because wrapping is not working :/
-readline.parse_and_bind('set colored-completion-prefix On') # colors types for TAB autocompletion.
-readline.parse_and_bind('set colored-stats On') # colored tab-autocomplete file names (LS_COLORS)
-    #readline.parse_and_bind('set completion-display-width 2') # columns to display auto completion options available # Not Working
-readline.parse_and_bind('set expand-tilde On') # expand tilde? # Not working, I do this manually.
-readline.parse_and_bind('set history-preserve-point On') # set the cursor point in history.
-readline.parse_and_bind('set match-hidden-files On')
-    #readline.parse_and_bind('set page-completions On')
-    #readline.parse_and_bind('set print-completions-horizontally On')
-readline.parse_and_bind('set show-all-if-ambiguous On')
-    #readline.parse_and_bind('set skip-completed-text On')
-    #readline.parse_and_bind('set visible-stats On')
-readline.parse_and_bind('set mark-directories On') # for appending a slash # supposedly "On" by default, but not working.
-
+###===========================================
+## PRELIMINARY FILE/DESCRIPTOR WORK:
+###===========================================
+if not os.path.exists(os.path.join(os.path.expanduser("~"),".dps")): # create the directory if it does not exist
+    os.mkdir(os.path.join(os.path.expanduser("~"),".dps")) # mkdir
+# Set up the log file itself:
+if not os.path.exists(LOG_FILENAME):
+    with open(LOG_FILENAME,'a') as log_file:
+        log_file.write("When,Host,Network,Who,Where,What\n")
+# Set up the config file/pull values:
+if not os.path.exists(CONFIG_FILENAME):
+    # Add the file
+    with open(CONFIG_FILENAME,'a') as config_file:
+        ### ADD ALL CONFIG STUFF HERE:
+        config_file.write("[Style]\n")
+        config_file.write("PRMPT_STYL = 0\n")
+        print(f"{bcolors.FAIL}[!] Configuration file generated, please restart shell.{bcolors.ENDC}")
+        sys.exit(1)
+else:
+    # Config file exists, grab the values using configparser:
+    CONFIG.read(CONFIG_FILENAME) # read the file
+    CONFIG.sections() # get all sections of the config
+    if 'Style' in CONFIG:
+        print(f"CONFIG['Style']['PRMPT_STYL']: "+CONFIG['Style']['PRMPT_STYL'])
+        PRMPT_STYL = int(CONFIG['Style']['PRMPT_STYL']) # grab the value of the style
+    else:
+        print(f"{bcolors.FAIL}[!]{bcolors.ENDC} Error in config file")
+        sys.exit() # die
+    # TODO else: # add it
+# Get the adapter and IP address:
+for adapter in ADAPTERS: # loop through adapters
+    if re.match("^e..[0-9]+",adapter.nice_name):
+        NET_DEV = adapter.nice_name+":"+adapter.ips[0].ip
 def log_cmd(cmd): # logging a command to the log file:
     with open(LOG_FILENAME,'a') as log_file:
         log_file.write(str(datetime.datetime.now())+","+HOSTNAME+","+str(NET_DEV)+","+UID+","+os.getcwd()+","+cmd+"\n")
@@ -98,7 +110,7 @@ def help(cmd_name):
        e.g: %f.%L123@client.org
        result: j.doe123@client.org
                     """)
-        else:
+        elif cmd_name == "dps_wifi_mon":
             print("""
     -- \033[1mDPS Wi-Fi Monitor Mode\033[0m --
 
@@ -108,6 +120,12 @@ def help(cmd_name):
       • \033[1m\033[94miw\033[0m
       • \033[1m\033[94mairmon-ng\033[0m
       • \033[1m\033[94mifconfig\033[0m
+            """)
+        elif cmd_name == "dps_config":
+            print("""
+        -- \033[1mDPS Configuration Settings\033[0m --
+
+          \033[1m\033[94mdps_config \033[0mprompt (integer (0-9))
             """)
     else:
         print("""
@@ -131,6 +149,9 @@ def help(cmd_name):
         """)
     shell() # return to our shell() function to capture more input.
 
+###===========================================
+## COMMAND HOOKS:
+###===========================================
 def run_cmd(cmd): # run a command. We capture a few and handle them, like "exit","quit","cd","sudo",etc:
     cmd_delta = cmd # the delta will be mangled user input as we see later:
     cmd_delta = re.sub("~",os.path.expanduser("~"),cmd_delta)
@@ -162,7 +183,10 @@ def run_cmd(cmd): # run a command. We capture a few and handle them, like "exit"
         shell()
     elif(cmd_delta.startswith("dps_config")):
         args = re.sub("dps_config","",cmd_delta).split() # make an array
-        dps_config(args)
+        if len(args) > 1:
+            dps_config(args)
+        else:
+            help("dps_config")
     elif(cmd_delta.startswith("help")):
         args = cmd_delta.split()
         if(len(args)>1):
@@ -199,27 +223,31 @@ def run_cmd(cmd): # run a command. We capture a few and handle them, like "exit"
         subprocess.call(["/bin/bash", "--init-file","/root/.bashrc", "-c", cmd_delta])
     shell() # or else return to shell
 
-def exit_gracefully(): # handle CTRL+C or CTRL+D, or quit, or exit gracefully:
-        #ans = input(bcolors.FAIL+"\n[!] CTRL+C DETECTED\n[?] Do you wish to quit the Demon Pentest Shell (y/n)? "+bcolors.ENDC)
-        ans = input(bcolors.FAIL+"\n[?]"+bcolors.ENDC+" Do you wish to quit the Demon Pentest Shell (y/n)? ")
-        if ans == "y":
-            print("[+] Quitting Demon Penetst Shell. File logged: "+LOG_FILENAME)
-            sys.exit(1)
-        else:
-            shell()
-
 ###===========================================
 ## DPS CUSTOM BUILT-IN SHELL CMD METHODS:
 ###===========================================
+def dps_update_config(args):
+    global CONFIG # declare the global
+    global CONFIG_FILENAME # config file name from global
+    if len(args) > 1:
+        if args[0] == "prompt": # set it in the config file:
+            #try:
+            print(f"{bcolors.OKBLUE}[*]{bcolors.ENDC} Adding "+str(args[1])+" as PRMPT_STYL in "+CONFIG_FILENAME)
+            CONFIG.set('Style','PRMPT_STYL',args[1]) # TODO int() ?
+            with open(CONFIG_FILENAME, 'w') as config_file:
+                CONFIG.write(config_file)
+            #except:
+                #print(f"{bcolors.FAIL}[!] ERROR setting value in ini file.{bcolors.ENDC}")
+
 def dps_config(args): # configure the shell
-    global PRMPT_CLR # this is the prompt color setting
-    print (f" \n-- {bcolors.BOLD}DPS Configuration{bcolors.ENDC} -- \n")
+    global PRMPT_STYL # this is the prompt color setting
     if args[0] == "prompt" and args[1] != "":
-        PRMPT_CLR = int(args[1])
+        PRMPT_STYL = int(args[1])
+        # Now set it for session / preference in the dps.ini file:
+        dps_update_config(args)
     else:
         print("Usage: dps_config prompt [0-9] for new prompt.")
     shell()
-
 def dps_wifi_mon(dev): # set an AC device into monitor mode using iw
     print("Set device "+dev+" into RFMON monitor mode.")
 # stats for shell logging
@@ -233,12 +261,6 @@ def dps_stats():
         line_count += len(open(os.path.expanduser("~/.dps/")+file).readlines())
     print("  • Total entries: "+bcolors.BOLD+bcolors.OKBLUE+str(line_count)+bcolors.ENDC+"\n")
 def dps_uid_gen(fs,csv_file): # take a CSV and generate UIDs using a format specifier from the user
-    # Usage:
-    # Use the following syntax, see case sensitivity:
-    # format_specifier = "%f.%L" # (first initial).(Last name)
-    # format_specifier = "%F.%L" # (First name).(Last name)
-    # format_specifier = "%F.%l@client.org" # (First name).(last initial)
-    # format_specifier = "%F.%L123@client.org" # (First name).(last initial)
     try:
         with open(csv_file) as nfh: # names file handle
             for line in nfh: # loop over each line
@@ -254,6 +276,18 @@ def dps_uid_gen(fs,csv_file): # take a CSV and generate UIDs using a format spec
     except:
         print(bcolors.FAIL+"[!]"+bcolors.ENDC+" Could not open file: "+csv_file+" for reading.")
     shell() # return to shell
+
+###===========================================
+## GENERAL METHODS FOR HANDLING THINGS:
+###===========================================
+def exit_gracefully(): # handle CTRL+C or CTRL+D, or quit, or exit gracefully:
+        #ans = input(bcolors.FAIL+"\n[!] CTRL+C DETECTED\n[?] Do you wish to quit the Demon Pentest Shell (y/n)? "+bcolors.ENDC)
+        ans = input(bcolors.FAIL+"\n[?]"+bcolors.ENDC+" Do you wish to quit the Demon Pentest Shell (y/n)? ")
+        if ans == "y":
+            print("[+] Quitting Demon Penetst Shell. File logged: "+LOG_FILENAME)
+            sys.exit(1)
+        else:
+            shell()
 
 def list_folder():
     PATHS=os.getenv('PATH').split(":")
@@ -271,19 +305,9 @@ def list_folder():
     """
     Our custom completer function
     """
-#    if text == "~/":
-#        text = os.path.expanduser("~/")
-#    options = [x for x in list_folder(text) if x.startswith(text)]
-#    return options[state]
-
-# REQUIRED:
-#readline.set_completer()
-#readline.set_completer(completer)
-#readline.parse_and_bind('tab: complete')
-#readline.set_completer_delims('~ \t\n`!@#$%^&*()-=+[{]}\\|;:\'",<>?')
-#readline.set_completer_delims(' \t\n')
-
+session = PromptSession() # session is global
 def shell():
+    global session
     try:
         # Build out the prompt fo rthe user:
         PATHS=os.getenv('PATH').split(":")
@@ -291,23 +315,23 @@ def shell():
             prompt_tail = "# "
         else:
             prompt_tail = "> " # added promt indicator for root
-        if PRMPT_CLR == 0:
+        if PRMPT_STYL == 0: # Matt's Prompt:
             prompt_txt = f"{bcolors.FAIL}{bcolors.BOLD}{UID}@{HOSTNAME}{bcolors.ENDC}:{bcolors.OKBLUE}{bcolors.BOLD}{os.getcwd()}{bcolors.WARNING}(dps){bcolors.ENDC}{prompt_tail}"
-        elif PRMPT_CLR == 1:
+        elif PRMPT_STYL == 1: # Doug's prompt:
             prompt_txt = f"{bcolors.BOLD}{UID}{bcolors.ENDC}{bcolors.WHT}@{bcolors.ENDC}{bcolors.LGHTGRY}{HOSTNAME}{bcolors.ENDC}{bcolors.BOLD}{bcolors.WHT}:{bcolors.ENDC}{bcolors.LGHTGRY}{os.getcwd()}{bcolors.BOLD}{bcolors.WHT}(dps){prompt_tail}{bcolors.ENDC}"
-        else:
+        else: # Default prompt:
             prompt_txt = f"{bcolors.FAIL}{bcolors.BOLD}{UID}@{HOSTNAME}{bcolors.ENDC}:{bcolors.OKBLUE}{bcolors.BOLD}{os.getcwd()}{bcolors.WARNING}(dps){bcolors.ENDC}{prompt_tail}"
 
-        #prompt_txt = UID+"@"+HOSTNAME+":"+os.getcwd()+"(dps)"+prompt_tail
-        #last_string = input(UID+bcolors.BOLD+"@"+bcolors.ENDC+HOSTNAME+bcolors.BOLD+"["+bcolors.ENDC+os.getcwd()+bcolors.BOLD+"]"+">> "+bcolors.ENDC)
         tab_complete=WordCompleter(list_folder())
-        last_string = prompt(ANSI(prompt_txt),completer=tab_complete) # fixes text-wrapping issue experenced with input()
+        last_string = session.prompt(ANSI(prompt_txt),completer=tab_complete) # fixes text-wrapping issue experenced with input()
         run_cmd(last_string)
     except KeyboardInterrupt:
         exit_gracefully()
     except EOFError:
         exit_gracefully()
 
+###===========================================
+## START THE SHELL:
+###===========================================
 print(bcolors.BOLD+"\n *** Welcome to the Demon Pentest Shell ("+VERSION+")\n *** Type \"exit\" to return to standard shell.\n"+bcolors.ENDC)
-
 shell() # start the app
