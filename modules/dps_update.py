893e7635f97f4502191b15ac8861622ca8b1e4a4
Douglas Berdeaux <weaknetlabs@gmail.com>
2021-03-03 16:30:06 -0500

    Fixed styling issue with dps_update

--- modules/dps_update.py
+++ modules/dps_update.py
@@ -21,6 +21,6 @@ def app(session,prompt_ui):
         g = git.cmd.Git(session.dps_install_dir)
         g.stash('save')
         g.pull(force=True)
-        print(f"\n{BOLD} ▹ {ENDC} {OKGREEN}Successfully pulled changes to local repository: {session.dps_install_dir} {ENDC}\n")
+        print(f"\n{OKGREEN}Successfully pulled changes to local repository: {session.dps_install_dir} {ENDC}\n")
     except:
         print(f"{FAIL} ✖ Something went wrong when trying to perform git operations. {ENDC}")
