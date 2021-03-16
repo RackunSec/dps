3eb36059031a786445567244f295b8f9f99a145d
Douglas Berdeaux <weaknetlabs@gmail.com>
2021-03-16 16:10:14 -0400

    Added --show-all to dps_config to set prompt theme

--- modules/dps_env.py
+++ modules/dps_env.py
@@ -75,6 +75,7 @@ def prompt(args,dpsrc,prompt_ui):
     WARN=prompt_ui.bcolors['WARN']
     ENDC=prompt_ui.bcolors['ENDC']
     BOLD=prompt_ui.bcolors['BOLD']
+    QUES=prompt_ui.bcolors['QUESTION']
     OKGREEN=prompt_ui.bcolors['OKGREEN']
     FAIL=prompt_ui.bcolors['FAIL']
     if len(args) > 1:
@@ -87,3 +88,41 @@ def prompt(args,dpsrc,prompt_ui):
             with open(dpsrc.dps_config_file, 'w') as config_file:
                 dpsrc.configparser.write(config_file)
             print(f"{OKGREEN}Prompt setting updated. Restart DPS to take effect.")
+    elif len(args)==1:
+        if args[0] == "--show": # Show the current theme name.
+            print(f"{OKGREEN}Theme Selection: {BOLD}{dpsrc.prompt_theme}{ENDC}")
+            print(f"{OKGREEN}Theme Name: {BOLD}{theme_names(dpsrc.prompt_theme)}{ENDC}")
+        elif args[0] == "--show-all":
+            print(f"You can choose from the following themes:\n")
+            for key,val in theme_names("all").items():
+                print(f"[{str(key)}]: {BOLD}{val}{ENDC}")
+            ans = input(f"\n{QUES} Type only the number: ")
+            if int(ans) in theme_names("all"):
+                prompt(('prompt',ans),dpsrc,prompt_ui)
+                return
+            else:
+                print(f"{FAIL} {ans} not within the theme_names->all_themes object.")
+                return
+
+## All theme names:
+def theme_names(theme_int):
+    all_themes = {
+        0: "Default",
+        1: "Minimal Skull",
+        2: "Minimal 1",
+        3: "Minimal 2",
+        4: "Minimal 3",
+        5: "Nouveau",
+        6: "Daemo",
+        7: "Dropped",
+        8: "Brew",
+        9: "Athens",
+        10: "Japan",
+        11: "Polar Mint",
+    }
+    if theme_int == "all":
+        return all_themes
+    elif int(theme_int) in all_themes:
+        return all_themes[int(theme_int)]
+    else:
+        return f"Theme {theme_int} has no defined name in dps_env->theme_names."
